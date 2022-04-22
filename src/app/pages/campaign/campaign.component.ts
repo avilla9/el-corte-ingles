4b76350fc72929f81c6a5c582a5814f690c73c40
Armando Villanueva <armandojvilla9@gmail.com>
2022-04-22 04:11:50 -0400

    campaign mockup

--- src/app/pages/campaign/campaign.component.ts
+++ src/app/pages/campaign/campaign.component.ts
@@ -7,8 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CampaignComponent implements OnInit {
 
+  public data: any;
+
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() { }
 
 }
