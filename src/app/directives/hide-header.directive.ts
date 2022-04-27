24469dce00d4982a2cf904605ae72ee514b74e72
Armando Villanueva <armandojvilla9@gmail.com>
2022-04-27 08:33:39 -0400

    header scroll event fixed, is not shaking anymore

--- src/app/directives/hide-header.directive.ts
+++ src/app/directives/hide-header.directive.ts
@@ -15,24 +15,24 @@ export class HideHeaderDirective implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    this.header = this.header.el;
     this.domCtrl.write(() => {
-      this.renderer.setStyle(this.header, 'transition', 'margin-top 700ms');
+      this.renderer.setStyle(this.header, 'transition', 'margin-top 500ms');
     });
   }
 
   @HostListener('ionScroll', ['$event']) onContentScroll($event: any) {
-    if ($event.detail.scrollTop > this.lastY) {
-      this.domCtrl.write(() => {
-        this.renderer.setStyle(this.header, 'margin-top', `-${this.header.clientHeight}px`);
-      });
-    } else {
-      this.domCtrl.write(() => {
-        this.renderer.setStyle(this.header, 'margin-top', '0');
-      });
-    }
 
+    if ($event.detail.scrollTop < this.header.clientHeight) {
+      if ($event.detail.scrollTop > this.lastY) {
+        this.domCtrl.write(() => {
+          this.renderer.setStyle(this.header, 'margin-top', `-${this.header.clientHeight}px`);
+        });
+      } else {
+        this.domCtrl.write(() => {
+          this.renderer.setStyle(this.header, 'margin-top', '0');
+        });
+      }
+    }
     this.lastY = $event.detail.scrollTop;
   }
-
 }
