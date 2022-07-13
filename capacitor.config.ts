3ba63f1cd30b79e635fded587e639369156a2d64
amanape <stephanpsaras@gmail.com>
2022-07-13 18:05:57 +0200

    Make responsive for width 768 and above

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'io.abcdefghy.starter',
   appName: 'SECI Point',
   webDir: 'www',
   bundledWebRuntime: false
