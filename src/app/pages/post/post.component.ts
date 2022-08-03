1f525ed552fdc1c39c38a70a09e4293e5d0539c3
Armando Villanueva <armandojvilla9@gmail.com>
2022-08-03 12:47:27 -0400

    like and share post

--- src/app/pages/post/post.component.ts
+++ src/app/pages/post/post.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, ViewEncapsulation, Pipe } from '@angular/core';
 import { InAppBrowser } from '@awesome-cordova-plugins/in-app-browser/ngx';
 import { NavController } from '@ionic/angular';
 import { SafeHtmlPipe } from '../../safe-html.pipe';
+import { ReactionService } from '../../services/reaction.service';
+import { Share } from '@capacitor/share';
 
 @Component({
   selector: 'app-post',
@@ -16,10 +18,12 @@ export class PostComponent implements OnInit {
   constructor(
     private iab: InAppBrowser,
     public navCtrl: NavController,
+    private reactions: ReactionService,
   ) { }
 
   ngOnInit() {
     this.post = JSON.parse(localStorage.getItem("post"));
+    console.log(this.post);
   }
 
   ionViewDidEnter() {
@@ -32,5 +36,31 @@ export class PostComponent implements OnInit {
 
   interalRedirection(url) {
     this.navCtrl.navigateForward(url);
+  }
+
+  like(post, event) {
+    console.log('event', event);
+    var target = event.target || event.srcElement || event.currentTarget;
+    this.reactions
+      .doLike(post)
+      .subscribe((response) => {
+        console.log(response);
+        if (response > 0) {
+          target.setAttribute('class', 'icon md hydrated liked');
+          target.setAttribute('name', 'heart');
+        } else {
+          target.setAttribute('class', 'icon md hydrated');
+          target.setAttribute('name', 'heart-outline');
+        }
+      });
+  }
+
+  async shareApp(post) {
+    await Share.share({
+      title: post.title,
+      text: post.short_description,
+      url: 'https://app-eci.web.app/',
+      dialogTitle: 'Share with buddies',
+    });
   }
 }
