66c33abc7a3c5cf79d1c8c2c51f933b4e1fc6b18
GoldJackAnderson <goldjackanderson@gmail.com>
2023-01-27 00:56:40 -0400

    [UPDATE] API Call
    Se ha agregado el llamado a la API para el cambio de contraseña en sesion

--- src/app/pages/change-password/change-password.component.ts
+++ src/app/pages/change-password/change-password.component.ts
@@ -1,17 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-import { MenuController, NavController } from '@ionic/angular';
+import { LoadingController, MenuController, NavController } from '@ionic/angular';
 import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 import { HttpClientModule } from '@angular/common/http';
+import { NewPasswordServiceService } from 'src/app/services/new-password-service.service';
+
 @Component({
   selector: 'app-change-password',
   templateUrl: './change-password.component.html',
   styleUrls: ['./change-password.component.scss'],
 })
 export class ChangePasswordComponent implements OnInit {
+  error;
+  ok;
+  loading;
 
   constructor(
     public navCtrl: NavController,
     public menuCtrl: MenuController,
+    private changePasswords: NewPasswordServiceService,
+    private loadingCtrl: LoadingController,
   ) { }
 
   ngOnInit() {}
@@ -35,6 +42,35 @@ export class ChangePasswordComponent implements OnInit {
     return this.oldTextFieldType = !this.oldTextFieldType;
   }
 
-  
+  async send(info) {
+    let error = '';
+    if (error.length) {
+      console.log(error)
+      this.error = error;
+    } else {
+      const loading = await this.loadingCtrl.create({
+        message: 'Enviando...',
+        translucent: true,
+      });
+      await loading.present();
+      this.loading = true;
+      this.error = '';
+
+      this.changePasswords.send(info).subscribe((res: any) => {
+        // console.log(res.message);
+        loading.dismiss();
+        this.loading = false;
+        this.ok = res.message
+      }, (err: any) => {
+        loading.dismiss();
+        console.log(err);
+        this.loading = false;
+        let errorMessage = err.error.new_password[0]
+        console.log(errorMessage);
+        this.error = errorMessage;
+      });
+    }
+  }
+
 
 }
